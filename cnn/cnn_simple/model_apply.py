8d1843038f5758a5002ee36b006e4a34f2b03aec
gxrao <raogx1993@hotmail.com>
2018-04-01 10:57:45 +0800

    add file: model_apply.py

--- cnn/cnn_simple/model_apply.py
+++ cnn/cnn_simple/model_apply.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 # prepare data
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 mnist = input_data.read_data_sets('../../MNIST/',one_hot=True)
+
 saver = tf.train.import_meta_graph(r'./cnn/cnn_simple/model/mnist.ckpt.meta') 
 sess = tf.InteractiveSession()
 saver.restore(sess, r'./cnn/cnn_simple/model/mnist.ckpt')  
